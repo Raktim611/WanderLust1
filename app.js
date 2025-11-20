649f0c6c145eeb53edfa318328a94798ec63b63f
Raktim611 <raktimnandi35@gmail.com>
2025-11-20 16:31:27 +0530

    Updated

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const sessionOptions={
     saveUninitialized:true,
     cookie:
     {
-        secure:true,
+        secure: process.env.NODE_ENV === "production",
         expires:Date.now()+7*24*60*60*1000,
         maxAge:7*24*60*60*1000,
         httpOnly:true
